0f888d0418247232c5c82af862e3fdb54c5709fd
Raymart <raymartformalejo00@gmail.com>
2023-08-02 15:20:53 +0800

    added vercel.json to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,9 +15,9 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
 
-      { path: "/admin/post", element: <WritePage /> },
+      { path: "admin/post", element: <WritePage /> },
       {
-        path: "/admin/topic",
+        path: "admin/topic",
         element: <TopicPage />,
       },
     ],
