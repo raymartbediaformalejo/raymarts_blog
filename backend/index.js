dc542d1813087ed769e1636a63a4d8513ef3997f
Raymart <raymartformalejo00@gmail.com>
2023-08-03 00:30:25 +0800

    COMBINE backend to client

---
+++ backend/index.js
@@ -0,0 +1,91 @@
+import express from "express";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+const db = mysql.createConnection(process.env.DATABASE_URL);
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("dist"));
+
+app.get("/api/topics", (req, res) => {
+  const q = "SELECT * FROM topic";
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+app.get("/api/posts", (req, res) => {
+  const q = "SELECT * FROM post";
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+app.post("/api/topics", (req, res) => {
+  const q = "INSERT INTO topic (`name`) VALUES (?)";
+  const values = [req.body.name];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(`Successfully added the ${values}`);
+  });
+});
+
+app.delete("/api/topics/:id", (req, res) => {
+  const topicId = req.params.id;
+  const q = "DELETE FROM topic WHERE id = ?";
+  db.query(q, [topicId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Topic has been deleted successfully.");
+  });
+});
+
+app.put("/api/topics/:id", (req, res) => {
+  const topicId = req.params.id;
+  const q = "UPDATE topic SET `name` = ? WHERE id = ?";
+  const values = [req.body.name];
+  db.query(q, [values, topicId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(`T has been updated successfully.`);
+  });
+});
+
+app.post("/api/posts", (req, res) => {
+  const q =
+    "INSERT INTO post (`category`, `topic`, `title`, `summary`, `coverPhoto`, `isFeatured`, `content`, `status`, `visibility`) VALUES (?)";
+  const values = [
+    req.body.category,
+    req.body.topic,
+    req.body.title,
+    req.body.summary,
+    req.body.coverPhoto,
+    req.body.isFeatured,
+    req.body.content,
+    req.body.status,
+    req.body.visibility,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.json(`Successfully added the post!`);
+    }
+  });
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
